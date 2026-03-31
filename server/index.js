80dc09294dcd23768f81abb8eacf8814a357897c
Vishnuraj2005 <vr926156@gmail.com>
2026-03-31 15:02:31 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,91 @@
+const express = require ('express')
+const mongoose = require ('mongoose')
+const cors = require ('cors')
+const app = express()
+
+app.use(cors({
+    origin:'http://localhost:3000',
+    methods:['PUT','GET','POST','DELETE']
+}))
+
+app.use(express.json())
+
+const FoodModel= require("./models/Food")
+mongoose.connect("mongodb+srv://admin:admin@cluster0.g0pbojz.mongodb.net/food")
+.then(()=>console.log('Connected'))
+.catch(err=>console.log(err))
+
+// INSERT DATA
+
+app.post("/insert",async(req,res)=>{
+    const {foodName,description}=req.body;
+    const food=new FoodModel({
+        foodName:foodName,
+        description:description
+    })
+    try
+    {
+       const result=await food.save();
+       res.send(result);
+       console.log(result);
+    }
+    catch(err)
+    {
+          console.log(err)
+    }
+})
+
+// GET ALL DATA
+
+app.get("/read",async(req,res)=>{
+    try{
+        const food=await FoodModel.find();
+        res.send(food);
+    }
+    catch(err){
+        res.send("error")
+    }
+})
+
+// UPDATA DATA
+
+app.put("/update",async(req,res)=>{
+    const {newFoodName,newDescription,id}=req.body;
+
+    try{
+        const updateFood=await FoodModel.findById(id);
+        if(!updateFood){
+            return res.status(400).send("Data not found")
+        }
+    
+    updateFood.foodName=newFoodName;
+    updateFood.description=newDescription;
+    await updateFood.save()
+    res.send("Data Updated....")
+}
+catch(err){
+    console.log(err);
+}
+})
+
+//Delete data
+
+app.delete("/delete/:id",async(req,res)=>{
+    const id=req.params.id;
+    try
+    {
+        const result=await FoodModel.findByIdAndDelete(id);
+        if(!result)
+        {
+            return res.status(404).send("Food item not found")
+        }
+        res.send("Food item delete")
+    }catch(err)
+        {
+            console.error(err)
+        }
+    })
+
+app.listen(3001,()=>{
+    console.log("Server is Running...")
+})
